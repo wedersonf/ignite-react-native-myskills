6a7cbbd4d9e7eafec34048ba2096317e6c077f0e
wedersonf <wedymf@gmail.com>
2021-12-09 23:44:22 -0300

    add splash and icon

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
- 
+import SplashScreen from 'react-native-splash-screen';
 import { Home } from './src/pages/Home';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" />
